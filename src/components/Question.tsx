38928e3a640eb11997c18df63f5fdf8046bb6fe7
Cyronite <prasunsharma@icloud.com>
2024-06-20 18:49:54 -0400

    minor bugs

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -2,11 +2,11 @@ function Question(props:{points:number[][], ratio:number[], attemptsTaken:number
 
     
     return (
-        <div className="flex justify-center items-center">
-          { (props.attemptsTaken != props.totalAttempts) && <p className="w-[70vw] text-center font-bree text-3xl">
+        <div className="flex justify-center items-center h-[5vh]">
+          { (props.attemptsTaken != props.totalAttempts) && <p className="w-[70vw] text-center font-bree text-lg">
             Given two points A ({props.points[0][0]}, {props.points[0][1]}) and B ({props.points[1][0]}, {props.points[1][1]}), find the coordinates of the point that divides the line segment AB in the ratio {props.ratio[0]}:{props.ratio[1]}.
           </p>}
-          { (props.attemptsTaken == props.totalAttempts) && <p className="w-[70vw] text-center font-bree text-3xl">
+          { (props.attemptsTaken == props.totalAttempts) && <p className="w-[70vw] text-center font-bree text-lg">
             Please select a mode and level then click start to begin!
           </p>}
         </div>
